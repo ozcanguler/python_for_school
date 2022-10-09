d54876a30dc9f53b520473a3d152d3ef726821f2
OzcanGuler <gulerozcan818@gmail.com>
2022-10-09 13:06:58 +0300

    init

--- 8/code/caesar_cipher.py
+++ 8/code/caesar_cipher.py
@@ -2,13 +2,11 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 def encrypt():   
     before_text=[]
-    after_text=[]
     bbbbb=""
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     while(not direction=="encode" and not direction=="decode"):
         direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     if(direction=="encode"):
-
         text = input("Type your message:\n").lower()
         shift = int(input("Type the shift number:\n"))
         for i in text:
@@ -17,7 +15,7 @@ def encrypt():
                     #before_text.append(k)               
                 if(i==alphabet[k]):
                     if(k+shift>25):
-                        before_text.append(alphabet[k+shift-25])
+                        before_text.append(alphabet[k+shift-26])
                     else:    
                         before_text.append(alphabet[k+shift])
             if(i not in alphabet):
@@ -31,12 +29,19 @@ def encrypt():
             bbbbb+=i
         print(bbbbb)
         direction_repeat = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n")
-        while(not direction_repeat=="yes" and not direction_repeat=="no"):
-            direction_repeat = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n")
+        if(direction_repeat=="yes"):
+            encrypt()
+        elif(direction_repeat=="no"):
+            print("GoodBye!")
+            quit()
+        else: 
+            while(not direction_repeat=="yes" and not direction_repeat=="no"):       
+                direction_repeat = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n")
         if(direction_repeat=="yes"):
             encrypt()
         if(direction_repeat=="no"):
             print("GoodBye!")
+            quit()                       
     if(direction=="decode"):
          text = input("Type your message:\n").lower()
          shift = int(input("Type the shift number:\n"))
@@ -48,7 +53,7 @@ def encrypt():
                shift=shift%26                
             if(i==alphabet[k]):
                 if(k-shift>25):
-                    before_text.append(alphabet[k-shift-25])
+                    before_text.append(alphabet[k-shift-26])
                 else:    
                     before_text.append(alphabet[k-shift])
         if(i not in alphabet):
@@ -62,42 +67,12 @@ def encrypt():
         bbbbb+=i
     print(bbbbb)
     direction_repeat = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n")
+    while(not direction_repeat=="yes" and not direction_repeat=="no"):
+            direction_repeat = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n")
     if(direction_repeat=="yes"):
         encrypt()
     if(direction_repeat=="no"):
         print("GoodBye!")                   
 encrypt()
-
-'''
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-def encrypt():   
-    before_text=[]
-    after_text=[]
-    bbbbb=""
-    #direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-    text = "qnuux fxaum!" #input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    for i in text:
-        for k in range(0,len(alphabet)):
-            #if(k not in alphabet):
-                #before_text.append(k)
-            if(shift>25):
-               shift=shift%26                
-            if(i==alphabet[k]):
-                if(k-shift>25):
-                    before_text.append(alphabet[k-shift-25])
-                else:    
-                    before_text.append(alphabet[k-shift])
-        if(i not in alphabet):
-            before_text.append(i)               
-
-    print(before_text)
-    #for i in before_text:
-    #    after_text.append(alphabet[i])
-    #print(after_text)
-    for i in before_text:
-        bbbbb+=i
-    print(bbbbb)
-encrypt()  
-'''                    
+               
               
