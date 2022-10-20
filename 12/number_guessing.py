fb0c89a3574f1946b2c65575c8892a9e69f982ce
OzcanGuler <gulerozcan818@gmail.com>
2022-10-20 18:07:01 +0300

    init

--- 12/number_guessing.py
+++ 12/number_guessing.py
@@ -1,6 +1,7 @@
+from mimetypes import guess_type
 from random import randint
 
-
+'''
 print("Welcome to the number guessing game\n I'm thinking of a number between 1 and 100.")
 
 def make_guess(guess_count,number):
@@ -35,3 +36,44 @@ def choose_level():
         choose_level()
 
 choose_level()
+
+'''
+EASY_LEVEL_TURNS=10
+HARD_LEVEL_TURNS=5
+
+
+def check_answer(guess,answer,turns):
+    ''' checks answer against guess. Returns the number of turns remaining'''
+    if guess>answer:
+        print("Too high.")
+        return turns-1
+    elif guess<answer:
+        print("Too low.")
+        return turns-1
+    else:
+        print(f"You got it! The answer was {answer}.")
+def set_difficulty():
+    level=input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if level=="easy":
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+def game():
+    print("Welcome to the number guessing game\nI'm thinking of a number between 1 and 100.")
+    answer=randint(1,100)
+    print(f"The correct answer is {answer}")
+
+    turns=set_difficulty()
+    
+
+    guess=0
+    while guess!=answer:
+        print(f"You have {turns} attempts remaining to guess the number")
+        guess = int(input("Make a guess:"))
+        turns=check_answer(guess,answer,turns)
+        if turns==0:
+            print("You've run out of guesses, you lose.")
+            return
+        elif guess!=answer:
+            print("Guess again")
+game()
