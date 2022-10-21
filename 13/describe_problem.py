efc54d68846d0d61163a2666b25cddb603554fbb
OzcanGuler <gulerozcan818@gmail.com>
2022-10-22 00:05:42 +0300

    init

---
+++ 13/describe_problem.py
@@ -0,0 +1,13 @@
+'''
+def my_function():
+    for i in range(0,21):
+        if i=20:
+            print("You got it")
+my_function()
+'''
+
+def my_function():
+    for i in range(0,21):
+        if i==20:
+            print("You got it")
+my_function()
