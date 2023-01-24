e83479aedb70bbb1ee0e56944b6df8837af52ce6
OzcanGuler <gulerozcan818@gmail.com>
2023-01-24 23:55:38 +0300

    control added

--- 20/snake.py
+++ 20/snake.py
@@ -1,21 +1,39 @@
 from turtle import Turtle
 positions=[(0,0),(-20,0),(-40,0)]#1
 move=20
+up=90 #5
+down=270 #5
+left=180 #5
+right=0 #5
+colors=("blue","red","green")
 class Snake:
-    def __init__(seif):
-        seif.segments=[]
-        seif.createSnake()
+    def __init__(self):
+        self.segments=[]
+        self.createSnake()
+        self.head=self.segments[0]  #4
     def createSnake(self):
-        for i in positions:#1
+        for i in range(0,3):#1
             my_snake=Turtle(shape="square")#1
             my_snake.penup()#2
-            my_snake.goto(i)#1
-            my_snake.color("white")#1
+            my_snake.goto(positions[i])#1
+            my_snake.color(colors[i])#1
             self.segments.append(my_snake)#2
     def move(self):
         for s in range(len(self.segments)-1,0,-1):#3
             new_x=self.segments[s-1].xcor()#3
             new_y=self.segments[s-1].ycor()#3
             self.segments[s].goto(new_x,new_y)#3
-        self.segments[0].forward(move) #3
-        self.segments[0].left(30)   #3  
+        self.head.forward(move) #3
+        #self.segments[0].left(30)   #3
+    def up(self):#4
+        if self.head.heading()!=down:
+            self.head.setheading(up)
+    def down(self):#4
+        if self.head.heading()!=up:
+            self.head.setheading(down)
+    def left(self):#4
+        if self.head.heading()!=right:
+            self.head.setheading(left)
+    def right(self):#4
+        if self.head.heading()!=left:
+            self.head.setheading(right)        
