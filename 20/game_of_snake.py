c441dd591a6766987787800a91f9a0827d17f39f
OzcanGuler <gulerozcan818@gmail.com>
2023-01-24 23:54:21 +0300

    control and food added

--- 20/game_of_snake.py
+++ 20/game_of_snake.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 import time
 from snake import Snake
+from food import Food 
 screen=Screen()#1
 screen.setup(width=600,height=600)#1
 screen.bgcolor("black")#1
@@ -8,6 +9,13 @@ screen.title("Snake Game")#1
 screen.tracer(0)#1
 
 snake=Snake()
+food=Food() #5
+
+screen.listen()
+screen.onkey(snake.up, "Up")    #4
+screen.onkey(snake.down, "Down")    #4
+screen.onkey(snake.left, "Left")    #4
+screen.onkey(snake.right, "Right")  #4
 
 is_game_start=True#2
 while is_game_start:#2
