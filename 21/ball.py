13fc115e3214b0cebe30796a813f53cbf820816c
OzcanGuler <gulerozcan818@gmail.com>
2023-01-28 15:07:57 +0300

    the direction of the ball should be random

--- 21/ball.py
+++ 21/ball.py
@@ -7,7 +7,8 @@ class Ball(Turtle):
         self.shapesize(stretch_len=0.5,stretch_wid=0.5) 
         self.color("purple")  
         self.x_move=10
-        self.y_move=10    
+        self.y_move=5
+        self.move_speed=0.05    
     def move(self): 
         new_x=self.xcor()+self.x_move
         new_y=self.ycor()+self.y_move
@@ -15,7 +16,14 @@ class Ball(Turtle):
     def bounce_y(self):
         self.y_move*=-1       
     def bounce_x(self):
-        self.x_move*=-1     
+        self.x_move*=-1 
+        self.move_speed*=0.9
+    def reset_ball_p(self):
+        self.move_speed=0.05
+        self.goto(0,0)
+        self.bounce_x()
+    def speed_increase(self):
+        self.x_move+=1     
 
         
           
