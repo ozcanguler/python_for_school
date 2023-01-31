d25bc0b06cdcf898aeecd15eaac786cb164b50c1
OzcanGuler <gulerozcan818@gmail.com>
2023-01-31 20:41:51 +0300

    save file added

--- 20/scoreboard.py
+++ 20/scoreboard.py
@@ -1,11 +1,17 @@
 from turtle import Turtle   #7
 ALIGMENT='center'   #7
 FONT=('Arial',15,'bold')    #7
+
+
+
 class Scoreboard(Turtle):   #7
     def __init__(self): #7
         super().__init__()  #7
+        with open("C:/Users/Garavel/Desktop/pythonsil/snake_game/son/data.txt",mode="r") as fileread:
+            self.high_score=fileread.read()
+            fileread.close()
         self.score=0    #7
-        self.high_score=0
+        #self.high_score=0
         self.goto(0,270)    #7
         self.color("white") #7
         self.update_scoreboard()    #7
@@ -22,10 +28,13 @@ class Scoreboard(Turtle):   #7
         self.update_scoreboard()    #7
     
     def reset(self):
-        if self.score>self.high_score:
-            self.high_score=self.score
-        self.score=0
-        self.update_scoreboard()
+        with open("C:/Users/Garavel/Desktop/pythonsil/snake_game/son/data.txt",mode="w") as filewrite:            
+            if self.score>int(self.high_score):
+                self.high_score=self.score
+                filewrite.write(f"{self.high_score}")
+                filewrite.close()
+            self.score=0
+            self.update_scoreboard()
     
     def game_over(self):    #8
         self.goto(0,0)
