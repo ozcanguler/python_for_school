e1b4cf1d6354210817992271f805878e27a704d7
OzcanGuler <gulerozcan818@gmail.com>
2023-02-11 23:51:43 +0300

    :(

---
+++ 25/natoAlphabet/main.py
@@ -0,0 +1,13 @@
+import pandas
+data = pandas.read_csv("/Users/Garavel/Desktop/pythonsil/natoAlphabet/nato_phonetic_alphabet.csv")
+
+#print(data.to_dict())
+
+phonetic_dict={row.letter:row.code for (index,row) in data.iterrows()}
+#print(phonetic_dict)
+word=input("Enter a word:").upper()
+output_list=[phonetic_dict[letter] for letter in word]
+print(output_list)
+
+
+
