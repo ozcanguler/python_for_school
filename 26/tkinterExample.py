1ee9675e13211b678f8adda31395c05b51fdf6cc
OzcanGuler <gulerozcan818@gmail.com>
2023-02-13 00:43:48 +0300

    I will go crazy 8ol

--- 26/tkinterExample.py
+++ 26/tkinterExample.py
@@ -7,7 +7,25 @@ window.minsize(width=600,height=600)
 
 
 label1=tkinter.Label(text="First Label", font=("Arial",24,"bold"),bg="navy",fg="yellow")
-label1.pack(side="right") #geometry-management mechanism
+#label1.pack(side="right") #geometry-management mechanism
+label1.pack()
+
+label1["text"]="New First Label"
+label1.config(text="Passport Please")
+
+
+
+def button1_clicked():
+    label1["text"]=input.get()
+
+button1=tkinter.Button(text="Click Me",command=button1_clicked)
+button1.pack()
+
+
+input= tkinter.Entry(width=30)
+input.pack()
+#label1.config(text=input.get())
+#label1["text"]=input.get()
 
 micky=t.Turtle()
 micky.penup()
